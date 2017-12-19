c73407e7961248db8ca4d7b466caa8093512bca5
gene-yoo <gene.y.yoo@gmail.com>
2017-12-19 16:06:38 -0500

    Working signup and render token

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,11 +1,34 @@
 class Api::V1::AuthController < ApplicationController
   def create
+    user = User.find_by(username: params[:user][:username])
+
+    if user && user.authenticate(params[:user][:password])
+      # issue user a token
+      render json: user
+    else
+      render json: {error: 'You must be playing yoself ... '}, status: 401
+    end
   end
 
   def show
+    token = request.headers['token']
+    decoded = JWT.decode(token, ENV['secret'], true, { algorithm: ENV['algo'] })
+    user = User.find_by(username: decoded[0]['username'])
+
+    if user
+      render json: user
+    else
+      render json: {error: 'You must be playing yoself ... '}, status: 401
+    end
   end
 
   def snake
+    user = User.find_by(username: params[:username])
+    if user
+      render json: user
+    else
+      render json: {error: 'You must be playing yoself ... '}, status: 401
+    end
   end
 
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
-    binding.pry
+    user = User.new({username: user_params['username'], password: user_params['password'], password_confirmation: user_params['passwordConfirmation']})
+
+    if user.valid?
+      user.save
+      token = JWT.encode({id: user.id}, ENV['secret'], ENV['algo'])
+      render json: { token: token, id: user.id, username: user.username }
+    else
+      render json: { error: 'You might rethink yoself.', status: 422 }
+    end
+
   end
 
   def update
@@ -13,6 +22,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
+    params.require(:user).permit(:username, :password, :passwordConfirmation)
   end
 
 end
