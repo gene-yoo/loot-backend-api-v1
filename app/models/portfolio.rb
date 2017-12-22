c2226815fb3113b0176b64c49dcb13121edf143d
gene-yoo <gene.y.yoo@gmail.com>
2017-12-22 10:13:55 -0500

    Start of day 12/22 10 AM

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -9,15 +9,8 @@ class Portfolio < ApplicationRecord
     budget = 2000.00
 
     params.map do |coinInfo|
-      coin = Coin.find_by(symbol: coinInfo[:coinSymbol])
-      if coin
-        coin
-      else
-        coin = Coin.create(name: coinInfo[:coinName], symbol: coinInfo[:coinSymbol])
-      end
-
+      coin = Coin.find_or_create_by(name: coinInfo[:coinName], symbol: coinInfo[:coinSymbol])
       quantity = (budget / coin_count) / coinInfo[:coinPrice]
-
       PortfolioCoin.create(trans_price: coinInfo[:coinPrice], trans_type: 'buy', quantity: quantity, paper_currency: 'USD', portfolio_id: self.id, coin_id: coin.id)
     end
   end
