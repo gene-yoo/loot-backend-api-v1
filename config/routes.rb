b8bd7889d0550ca0f5dc244e12ddd419c48e3824
gene-yoo <gene.y.yoo@gmail.com>
2017-12-28 20:04:35 -0500

    WIP token not coming through headers

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,15 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create, :update, :delete]
-      resources :portfolios, only: [:create, :show, :update, :delete]
+      resources :portfolios, only: [:create, :update, :delete]
 
       post '/signup', to: 'users#create', as: 'create_new_user'
 
       post '/login', to: 'auth#create', as: 'new_session'
       get '/current_user', to: 'auth#show', as: 'show_current_session'
       get '/snake', to: 'auth#snake', as: 'snake_path'
+
+      get '/portfolios', to: 'portfolios#show', as: 'get_portfolios'
     end
   end
 end
