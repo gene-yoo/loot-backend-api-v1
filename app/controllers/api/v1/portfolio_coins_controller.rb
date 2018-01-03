1635534d85f512d5d2d2f585f3d87bdaea3fdffe
gene-yoo <gene.y.yoo@gmail.com>
2018-01-03 05:05:38 -0500

    Working version Wed Jan 3 5AM

--- app/controllers/api/v1/portfolio_coins_controller.rb
+++ app/controllers/api/v1/portfolio_coins_controller.rb
@@ -4,8 +4,6 @@ class Api::V1::PortfolioCoinsController < ApplicationController
 
     coin = Coin.find_or_create_by(name: trans_params[:coinName], symbol: trans_params[:coinSymbol])
 
-    binding.pry
-
     quantity = trans_params[:transactionAmount] / trans_params[:transactionPrice]
 
     transaction = PortfolioCoin.new(trans_price: trans_params[:transactionPrice], trans_amt: trans_params[:transactionAmount], trans_type: trans_params[:transactionType], quantity: quantity, paper_currency:'USD', portfolio_id: portfolio.id, coin_id: coin.id)
@@ -13,7 +11,7 @@ class Api::V1::PortfolioCoinsController < ApplicationController
     if transaction.valid?
       transaction.save
 
-      if transaction.trans_type == 'buy'
+      if transaction.trans_type === 'buy'
         portfolio.balance -= trans_params[:transactionAmount]
       else
         portfolio.balance += trans_params[:transactionAmount]
