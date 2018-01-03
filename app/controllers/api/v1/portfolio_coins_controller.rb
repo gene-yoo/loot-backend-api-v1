4799d62d41df761d39228d7da41634af2fcb22c6
gene-yoo <gene.y.yoo@gmail.com>
2018-01-03 04:01:12 -0500

    Working version of selling modal

--- app/controllers/api/v1/portfolio_coins_controller.rb
+++ app/controllers/api/v1/portfolio_coins_controller.rb
@@ -4,13 +4,21 @@ class Api::V1::PortfolioCoinsController < ApplicationController
 
     coin = Coin.find_or_create_by(name: trans_params[:coinName], symbol: trans_params[:coinSymbol])
 
+    binding.pry
+
     quantity = trans_params[:transactionAmount] / trans_params[:transactionPrice]
 
     transaction = PortfolioCoin.new(trans_price: trans_params[:transactionPrice], trans_amt: trans_params[:transactionAmount], trans_type: trans_params[:transactionType], quantity: quantity, paper_currency:'USD', portfolio_id: portfolio.id, coin_id: coin.id)
 
     if transaction.valid?
       transaction.save
-      portfolio.balance -= trans_params[:transactionAmount]
+
+      if transaction.trans_type == 'buy'
+        portfolio.balance -= trans_params[:transactionAmount]
+      else
+        portfolio.balance += trans_params[:transactionAmount]
+      end
+
       portfolio.save
       render json: portfolio
     else
